26930c8513647e8dc8af96037d1a3bec96279e06
杨哲 <yangz@suirui.com>
2021-04-08 14:39:52 +0800

    Update test_sign.py
    更新方法调用

--- new_sign/test_sign.py
+++ new_sign/test_sign.py
@@ -5,12 +5,15 @@ import time
 import os
 import requests
 
-class test_sign:
-    def load(self):
-		file=self.path+'/config.json'
-		with open(file,'r') as f:
-			data=json.load(f)
-		return data
-    
+def load(self):
+    file=self.path+'/config.json'
+    with open(file,'r') as f:
+        data=json.load(f)
+    return data
+
 def get_new_ip(self):
-		return requests.get(url=self.ip).text
+    return requests.get(url=self.ip).text
+
+def __init__(self,Id,Token):
+    get_new_ip(self)
+    
