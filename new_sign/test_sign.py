7cb3848e801cd3f21364e91cb45a182caf7c8a0e
杨哲 <yangz@suirui.com>
2021-04-09 17:54:04 +0800

    Update test_sign.py
    修改解析方式

--- new_sign/test_sign.py
+++ new_sign/test_sign.py
@@ -17,7 +17,7 @@ def get_new_ip(self):
 def __init__(self,Id,Token):
     get_new_ip(self)
     self.login_token=str(Id)+","+str(Token)
-    self.format="json"
+    self.format="xml"
     self.lang="cn"
     self.headers={"User-Agent":"ddns_for_raspberry_pi/0.1(heeeepin@gmail.com)"}
     self.api='https://dnsapi.cn/'
@@ -25,4 +25,3 @@ def __init__(self,Id,Token):
     self.data={"login_token":self.login_token,"format":self.format,"lang":self.lang}
     self.path=os.path.dirname(os.path.realpath(__file__))
     self.domain_list=self.load()
-    
