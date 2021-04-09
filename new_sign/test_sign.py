a77410f6c9df70e73021e9fba6fb67a23507666f
杨哲 <yangz@suirui.com>
2021-04-09 17:53:40 +0800

    Update test_sign.py

--- new_sign/test_sign.py
+++ new_sign/test_sign.py
@@ -16,4 +16,13 @@ def get_new_ip(self):
 
 def __init__(self,Id,Token):
     get_new_ip(self)
+    self.login_token=str(Id)+","+str(Token)
+    self.format="json"
+    self.lang="cn"
+    self.headers={"User-Agent":"ddns_for_raspberry_pi/0.1(heeeepin@gmail.com)"}
+    self.api='https://dnsapi.cn/'
+    self.ip='http://members.3322.org/dyndns/getip'
+    self.data={"login_token":self.login_token,"format":self.format,"lang":self.lang}
+    self.path=os.path.dirname(os.path.realpath(__file__))
+    self.domain_list=self.load()
     
