900ede378c87c42b60ede15d175607930c5bada4
杨哲 <yangz@suirui.com>
2021-07-27 10:45:16 +0800

    Update test_sign.py
    更新django变量

--- new_sign/test_sign.py
+++ new_sign/test_sign.py
@@ -24,7 +24,15 @@ INSTALLED_APPS = [
     'SignIn',
 ]
 
-
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
 
 def load(self):
     file=self.path+'/config.json'
