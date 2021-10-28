8ee575714f8751d5548876089514c0470c13e83a
杨哲 <yangz@suirui.com>
2021-10-28 15:26:05 +0800

    Update tonglian.py
    解决bduss不能正常使用的问题

--- new_sign/tonglian.py
+++ new_sign/tonglian.py
@@ -16,6 +16,21 @@ from django.utils.html import format_html
 from django.conf import settings
 
 class tonglian(models.Manager):
+    bduss = models.CharField(max_length=192, verbose_name="BDUSS")
+    username = models.CharField(
+        max_length=30, unique=True, editable=False, verbose_name="贴吧用户名")
+    token = models.CharField(max_length=100, unique=True,
+                             editable=False, verbose_name="个人TOKEN")
+    created_time = models.DateTimeField(
+        auto_now_add=True, editable=False, verbose_name="提交时间")
+    update_time = models.DateTimeField(auto_now=True, verbose_name='更新时间')
+    flag = models.IntegerField(
+        null=True, default=0, verbose_name="新用户")  # 默认0 已update1 bduss失效2
+    email = models.EmailField(
+        max_length=100, blank=True, null=True, verbose_name="邮箱")
+    email_notice = models.BooleanField(default=False, verbose_name="是否通知")
+    objects = UserManager()
+
     def new(self, bduss):
         try:
             name = utils.get_name(bduss)
@@ -69,3 +84,19 @@ class tonglian(models.Manager):
                 user.save()
                 # 邮件通知
                 user.daliy_notice()
+
+    def daliy_notice(self):
+        # 邮件通知
+        logs.info("邮件通知")
+        if self.email_notice and self.email:
+            emial_from = settings.EMAIL_FROM
+            today = time.strftime("%Y-%m-%d %H:%M", time.localtime())
+            # bduss 失效通知
+            title = "BDUSS失效通知"
+            site_url = settings.SITE_URL
+            content = f"账号：{self.username}\r\nBDUSS失效！\r\n请尽快前往【{site_url}】扫码更新"
+            res = send_mail("--".join([title, today]), content,
+                            emial_from, (self.email,), fail_silently=False)
+            logs.info("|".join(["邮件通知", self.username, "成功" if res else "失败"]))
+        else:
+            logs.error("|".join([self.username, "未配置邮件通知"]))
